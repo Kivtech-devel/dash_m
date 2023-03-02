40ce4b7a992eb6930a83cae44ba68bc4da6cd36c
siddht1 <rt1yrru+github@gmail.com>
2023-03-02 12:37:14 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+//apprearance
+$("input.variation").on("click", function() {
+	if ($(this).val() > 3) {
+		$("body").css("background", "#111");
+		$("footer").attr("class", "dark");
+	} else {
+		$("body").css("background", "#f9f9f9");
+		$("footer").attr("class", "");
+	}
+});
+
+console.log(' Not yet connected to API');
